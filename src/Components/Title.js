a9097427022d1f1c34ab073128487a9b73645616
jondeh <dehlinjd@gmail.com>
2020-09-30 23:33:45 -0600

    added styling to landing page

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import '../SCSS/Title.scss';
 
 const Title = () => {
     return (
         <div className="title-container">
             <h1>Jonathon Dehlin</h1>
-            <h2>Software Solutions Engineer</h2>
+            <h2>Software Developer</h2>
         </div>
     )
 }
